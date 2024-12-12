ea753e384bad82a13db1fb00004fd0c458c77490
lizabez <73614632+lizabez@users.noreply.github.com>
2024-12-12 12:03:12 +0300

    Update child1.c

--- child1.c
+++ child1.c
@@ -4,27 +4,24 @@
 void convert_to_uppercase(FILE *input, FILE *output) {
     char ch;
     while ((ch = fgetc(input)) != EOF) {
-        fputc(toupper(ch), output);
+        fputc(toupper(ch), output); 
     }
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
 
     FILE *input = fopen(argv[1], "r");
     if (input == NULL) {
-        perror("Error opening input file");
-        return 1;
+        return 2;
     }
 
     FILE *output = fopen(argv[2], "w");
     if (output == NULL) {
-        perror("Error opening output file");
         fclose(input);
-        return 1;
+        return 3;
     }
 
     convert_to_uppercase(input, output);
