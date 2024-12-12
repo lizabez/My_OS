ec96144ab0dc7ce76ec954e08d224c8d31639966
lizabez <73614632+lizabez@users.noreply.github.com>
2024-12-12 12:02:23 +0300

    Update parent.c

--- parent.c
+++ parent.c
@@ -6,22 +6,22 @@ void run_child(const char *child_exe, const char *input_file, const char *output
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi;
     
-   
+    
     char command[1024];
     snprintf(command, sizeof(command), "\"%s\" \"%s\" \"%s\"", child_exe, input_file, output_file);
     
-
+   
     si.cb = sizeof(si);
     
 
     if (!CreateProcess(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
-        fprintf(stderr, "Error starting process %s\n", child_exe);
         exit(1);
     }
     
+ 
     WaitForSingleObject(pi.hProcess, INFINITE);
     
-
+ 
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
 }
@@ -31,15 +31,12 @@ int main() {
     const char *child1_output = "intermediate.txt";
     const char *child2_output = "output.txt";
 
-    // Запуск child1
-    printf("Запуск child1...\n");
+ 
     run_child("child1.exe", input_file, child1_output);
     
-    // Запуск child2
-    printf("Запуск child2...\n");
+
     run_child("child2.exe", child1_output, child2_output);
 
-    printf("Преобразования завершены. Результат в файле %s\n", child2_output);
 
     return 0;
 }
