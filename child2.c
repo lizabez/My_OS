11dce75eec4a45e2de83b47fc46d949b94b986e8
lizabez <73614632+lizabez@users.noreply.github.com>
2024-12-12 09:31:04 +0300

    Update child2.c

--- child2.c
+++ child2.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
 
-#define BUFFER_SIZE 1024
-
-int main() {
-    HANDLE hPipe2Read;
-    char buffer[BUFFER_SIZE];
-    DWORD bytesRead;
-
-    // Получение дескриптора pipe
-    hPipe2Read = GetStdHandle(STD_INPUT_HANDLE);
-
-    while (1) {
-        if (ReadFile(hPipe2Read, buffer, BUFFER_SIZE, &bytesRead, NULL) == FALSE) break;
-
-        // Заменяем пробелы на подчеркивания
-        for (int i = 0; i < bytesRead; i++) {
-            if (buffer[i] == ' ') {
-                buffer[i] = '_';
-            }
+void replace_spaces_with_underscore(FILE *input, FILE *output) {
+    char ch;
+    while ((ch = fgetc(input)) != EOF) {
+        if (ch == ' ') {
+            fputc('_', output); 
+        } else {
+            fputc(ch, output);
         }
-
-        // Выводим результат
-        printf("%s\n", buffer);
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL) {
+        perror("Error opening input file");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL) {
+        perror("Error opening output file");
+        fclose(input);
+        return 1;
+    }
+
+    replace_spaces_with_underscore(input, output);
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
