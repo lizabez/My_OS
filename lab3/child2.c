f65bd6567314bf8be3d62653d7b79f9b2da03cf6
lizabez <73614632+lizabez@users.noreply.github.com>
2024-12-25 21:55:28 +0300

    Create child2.c

---
+++ lab3/child2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define SHM_NAME "/shared_memory"
+#define SEM_EMPTY "/sem_empty"
+#define SEM_FULL "/sem_full"
+#define BUFFER_SIZE 256
+
+struct SharedData {
+    char message[BUFFER_SIZE];
+    int flag;
+};
+
+int main() {
+   
+    sem_t *sem_empty = sem_open(SEM_EMPTY, 0);
+    sem_t *sem_full = sem_open(SEM_FULL, 0);
+
+    if (sem_empty == SEM_FAILED || sem_full == SEM_FAILED) {
+        perror("sem_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct SharedData *shmaddr = mmap(NULL, sizeof(struct SharedData),
+                                      PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shmaddr == MAP_FAILED) {
+        perror("mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_wait(sem_empty);
+
+    for (int i = 0; shmaddr->message[i] != '\0'; i++) {
+        if (shmaddr->message[i] == ' ') {
+            shmaddr->message[i] = '_';
+        }
+    }
+
+    shmaddr->flag = 2;
+
+    sem_post(sem_full);
+
+    munmap(shmaddr, sizeof(struct SharedData));
+
+    return 0;
+}
