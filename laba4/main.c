15f3bf36bdcd5491b0c459efd2d882489d23d74d
lizabez <73614632+lizabez@users.noreply.github.com>
2025-01-11 16:13:30 +0300

    Update main.c

--- laba4/main.c
+++ laba4/main.c
@@ -1,83 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
+#include <stddef.h>
 #include <sys/time.h>
+
+
+typedef struct FreeBlock {
+    size_t size;           
+    struct FreeBlock *next; 
+} FreeBlock;
+
 
 typedef struct Allocator {
     void *memory;
     size_t size;
-    void* (*alloc)(struct Allocator *allocator, size_t size);
-    void (*free)(struct Allocator *allocator, void *ptr);
-    void (*destroy)(struct Allocator *allocator);
+    FreeBlock *free_list; 
     size_t allocated_memory;
     size_t freed_memory;
 } Allocator;
 
-double get_time();
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <allocator_library_path>\n", argv[0]);
-        return EXIT_FAILURE;
+
+
+
+Allocator* allocator_create(void *memory, size_t size) {
+    if (size < sizeof(FreeBlock)) {
+        fprintf(stderr, "Error: Not enough memory to create allocator.\n");
+        return NULL;
     }
 
-    const char *lib_path = argv[1];
-    void *handle = dlopen(lib_path, RTLD_LAZY);
-    if (!handle) {
-        fprintf(stderr, "Error loading library: %s\n", dlerror());
-        return EXIT_FAILURE;
+    FreeBlock *initial_block = (FreeBlock*)memory;
+    initial_block->size = size - sizeof(FreeBlock);
+    initial_block->next = NULL;
+
+    Allocator *allocator = (Allocator*)malloc(sizeof(Allocator));
+    if (!allocator) {
+        fprintf(stderr, "Error: Failed to allocate memory for allocator structure.\n");
+        return NULL;
     }
 
-    Allocator* (*create_allocator)(void *memory, size_t size) = dlsym(handle, "create_allocator");
-    void (*destroy_allocator)(Allocator *allocator) = dlsym(handle, "destroy_allocator");
-    void* (*alloc_memory)(Allocator *allocator, size_t size) = dlsym(handle, "alloc_memory");
-    void (*free_memory)(Allocator *allocator, void *ptr) = dlsym(handle, "free_memory");
+    allocator->memory = memory;
+    allocator->size = size;
+    allocator->free_list = initial_block;
+    allocator->allocated_memory = 0;
+    allocator->freed_memory = 0;
 
-    if (!create_allocator || !destroy_allocator || !alloc_memory || !free_memory) {
-        fprintf(stderr, "Error loading functions: %s\n", dlerror());
-        dlclose(handle);
-        return EXIT_FAILURE;
+    printf("Allocator created with size: %zu bytes\n", size);
+    return allocator;
+}
+
+
+void* allocator_alloc(Allocator *allocator, size_t size) {
+    if (size == 0) {
+        fprintf(stderr, "Error: Cannot allocate 0 bytes.\n");
+        return NULL;
+    }
+    size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
+
+    FreeBlock *prev = NULL;
+    FreeBlock *current = allocator->free_list;
+
+    while (current) {
+        if (current->size >= size) {
+            if (current->size > size + sizeof(FreeBlock)) {
+                FreeBlock *new_block = (FreeBlock*)((char*)current + sizeof(FreeBlock) + size);
+                new_block->size = current->size - size - sizeof(FreeBlock);
+                new_block->next = current->next;
+
+                current->size = size;
+                if (prev) {
+                    prev->next = new_block;
+                } else {
+                    allocator->free_list = new_block;
+                }
+            } else {
+                if (prev) {
+                    prev->next = current->next;
+                } else {
+                    allocator->free_list = current->next;
+                }
+            }
+
+            allocator->allocated_memory += size;
+            printf("Allocated block of size: %zu bytes\n", size);
+            return (void*)((char*)current + sizeof(FreeBlock));
+        }
+
+        prev = current;
+        current = current->next;
     }
 
-    size_t memory_size = 1024 * 1024;
-    void *memory = malloc(memory_size);
-    Allocator *allocator = create_allocator(memory, memory_size);
+    fprintf(stderr, "Error: Not enough memory to allocate %zu bytes.\n", size);
+    return NULL;
+}
 
-    double start, end;
 
-    start = get_time();
-    void *block1 = alloc_memory(allocator, 128);
-    end = get_time();
-    printf("Alloc time for 128 bytes: %.6f seconds\n", end - start);
+void allocator_free(Allocator *allocator, void *ptr) {
+    if (!ptr) {
+        fprintf(stderr, "Error: Cannot free a NULL pointer.\n");
+        return;
+    }
 
-    start = get_time();
-    void *block2 = alloc_memory(allocator, 256);
-    end = get_time();
-    printf("Alloc time for 256 bytes: %.6f seconds\n", end - start);
+    FreeBlock *block = (FreeBlock*)((char*)ptr - sizeof(FreeBlock));
+    allocator->freed_memory += block->size;
 
-    start = get_time();
-    free_memory(allocator, block1);
-    end = get_time();
-    printf("Free time for 128 bytes: %.6f seconds\n", end - start);
+    block->next = allocator->free_list;
+    allocator->free_list = block;
 
-    start = get_time();
-    free_memory(allocator, block2);
-    end = get_time();
-    printf("Free time for 256 bytes: %.6f seconds\n", end - start);
+    printf("Freed block of size: %zu bytes\n", block->size);
+}
 
-    double usage_factor = (double)allocator->allocated_memory / (double)memory_size;
-    printf("Memory usage factor: %.2f%%\n", usage_factor * 100);
 
-    destroy_allocator(allocator);
-    free(memory);
-
-    dlclose(handle);
-
-    return EXIT_SUCCESS;
+void allocator_destroy(Allocator *allocator) {
+    if (allocator) {
+        free(allocator);
+        printf("Allocator destroyed.\n");
+    } else {
+        fprintf(stderr, "Error: Invalid allocator.\n");
+    }
 }
 
 double get_time() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+
+int main() {
+    char memory_pool[1024];
+    Allocator *allocator = allocator_create(memory_pool, sizeof(memory_pool));
+
+    if (!allocator) {
+        return EXIT_FAILURE;
+    }
+
+    double start_time, end_time;
+
+    start_time = get_time();
+    void *block1 = allocator_alloc(allocator, 128);
+    void *block2 = allocator_alloc(allocator, 256);
+    end_time = get_time();
+    printf("Allocation time: %.6f seconds\n", end_time - start_time);
+
+    start_time = get_time();
+    allocator_free(allocator, block1);
+    allocator_free(allocator, block2);
+    end_time = get_time();
+    printf("Free time: %.6f seconds\n", end_time - start_time);
+
+    double usage_factor = (double)allocator->allocated_memory / allocator->size * 100;
+    printf("Memory usage factor: %.2f%%\n", usage_factor);
+
+    allocator_destroy(allocator);
+
+    return EXIT_SUCCESS;
 }
